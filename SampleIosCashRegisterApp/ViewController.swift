0e09fe33770c0d79da0bb89b2f062828e7797b0d
YutaKosuki <bambiyuta@gmail.com>
2016-01-06 13:06:16 +0900

    first commit

---
+++ SampleIosCashRegisterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleIosCashRegisterApp
+//
+//  Created by kosuki on 2016/01/06.
+//  Copyright © 2016年 kosuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
